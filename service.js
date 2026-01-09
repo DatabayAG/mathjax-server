c38e47507ce702aeec637dd7464a7b5d29575680
fneumann <fneumann@databay.de>
2026-01-09 10:58:31 +0100

    Move MathJax initialisation outside the request

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 /**
  * Server for rendering latex expressions
  * Adapted from https://github.com/mathjax/MathJax-demos-node/blob/master/simple/tex2svg
- * run with: node -r esm service.js
+ * run with: node service.js
  */
 
 /**
@@ -29,51 +29,49 @@ const math_css = [
   'use[data-c]{stroke-width:3px}'
 ].join('');
 
-const http = require('http');
-const qs = require('querystring');
-const port = 8003;
-const server = http.createServer(async (request, response) =>
-{
-  let body = '';
-  request.on('data', function (data) {
-    body += data;
-    if (body.length > 1e8) {
-      request.connection.destroy();
-    }
-  });
+main();
 
-  request.on('end', async function () {
-
-    try {
-      const post = JSON.parse(body);
-      let output = 'Unknown request';
-      if (post.math) {
-        output = await renderMath(math_config, post);
-      }
-
-      response.statusCode = 200;
-      response.setHeader('Content-Length', output.length);
-      response.end(output);
-    }
-    catch (error) {
-      console.log(error);
-      response.statusCode = 500;
-      response.end(error.message);
-    }
-  });
-});
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-/**
- * Render a single latex expression
- */
-async function renderMath(config, post) {
-  const MathJax = await require('mathjax').init(config);
-  const svg = MathJax.tex2svg(post.math);
+async function main() {
+  const MathJax = await require('mathjax').init(math_config);
   const adaptor = MathJax.startup.adaptor;
-  const html = adaptor.innerHTML(svg);
-  return html.replace(/<defs>/, `<defs><style>${math_css}</style>`)
+
+  const http = require('http');
+  const qs = require('querystring');
+  const port = 8003;
+  const server = http.createServer(async (request, response) =>
+  {
+    let body = '';
+    request.on('data', function (data) {
+      body += data;
+      if (body.length > 1e8) {
+        request.connection.destroy();
+      }
+    });
+
+    request.on('end', async function () {
+
+      try {
+        const post = JSON.parse(body);
+        let output = 'Unknown request';
+        if (post.math) {
+          const svg = MathJax.tex2svg(post.math);
+          const html = adaptor.innerHTML(svg);
+          output = html.replace(/<defs>/, `<defs><style>${math_css}</style>`)
+        }
+
+        response.statusCode = 200;
+        response.setHeader('Content-Length', output.length);
+        response.end(output);
+      }
+      catch (error) {
+        console.log(error);
+        response.statusCode = 500;
+        response.end(error.message);
+      }
+    });
+  });
+
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
